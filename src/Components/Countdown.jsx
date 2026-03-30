d3d7806b85288bd6a1d81a337e7c68055dbe846c
Anooj Shrestha <anoojshrestha.52532@gmail.com>
2026-03-30 15:08:51 +0200

    tata vinaju flight info added

--- src/Components/Countdown.jsx
+++ src/Components/Countdown.jsx
@@ -5,36 +5,17 @@ const Countdown = () => {
   return (
     <div className="w-full">
       <DisplayCountdown
-        name="अजा-अजि"
-        image="https://raw.githubusercontent.com/anoojshrestha/countdown/main/public/aja-aji.jpg"
-        dateTime="2024-08-08"
+        name="तता भिनाजु"
+        image="https://raw.githubusercontent.com/anoojshrestha/countdown/main/public/tata-vinaju.jpg"
+        dateTime="2026-04-06"
         tracking={[
           {
-            name: "Kathmandu to Istanbul (TK 0727)",
-            link: "https://www.turkishairlines.com/en-de/flights/flight-status/?t=1&flight=727&date=080824",
+            name: "Kathmandu to Delhi (AI 216)",
+            link: "https://www.flightaware.com/live/flight/AIC216",
           },
           {
-            name: "Istanbul to Nuremberg (TK 1505)",
-            link: "https://www.turkishairlines.com/en-de/flights/flight-status/?t=1&flight=1505&date=080824",
-          },
-        ]}
-      />
-      <DisplayCountdown
-        name="Ashis"
-        image="https://raw.githubusercontent.com/anoojshrestha/countdown/main/public/ashis.jpg"
-        dateTime="2024-08-22"
-        tracking={[
-          {
-            name: "Kathmandu to Singapore (SQ 441)",
-            link: "https://www.flightaware.com/live/flight/SIA441",
-          },
-          {
-            name: "Singapore to San Francisco (SQ 32)",
-            link: "https://www.flightaware.com/live/flight/SIA32",
-          },
-          {
-            name: "San Francisco to Austin (AS 534)",
-            link: "https://www.flightaware.com/live/flight/ASA534",
+            name: "Delhi to Frankfurt (AI 2029)",
+            link: "https://www.flightaware.com/live/flight/AIC2029",
           },
         ]}
       />
